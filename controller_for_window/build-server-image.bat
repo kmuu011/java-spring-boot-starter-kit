9d300864682d8c97bf6d7bfac0b4ceaa7be180a5
Hikari <kmuu011@naver.com>
2025-11-15 09:03:40 +0000

    ubuntu 개발 환경 구성성

--- controller_for_window/build-server-image.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-
-docker build -t production/spring-boot:latest -f .\docker-compose\java_spring_boot_starter_kit_prod\docker_file\spring\Dockerfile . & pause
--- controller_for_window/first-run-dev.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-cd docker-compose
-cd java_spring_boot_starter_kit_dev
-
-docker compose up -d
